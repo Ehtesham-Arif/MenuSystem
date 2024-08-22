2f9e8d0a0c821e8b920616355c7bd654c4910796
Ehtesham <ehtesham@carameltec.com>
2024-08-22 18:37:03 +0500

    - libs setup

---
+++ Assets/Reflex/Runtime/ProjectInstaller.cs
@@ -0,0 +1,13 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace Reflex.Runtime
+{
+	public class ProjectInstaller : MonoBehaviour, IInstaller
+	{
+		public void InstallBindings(ContainerBuilder containerBuilder)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
